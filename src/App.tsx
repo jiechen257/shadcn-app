d872c0d39bd2067363f2d59718b5f1bd4ee303d4
jiechen <1487503910@qq.com>
2023-10-20 14:04:43 +0800

    feat: add dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Button } from "@/components/ui/button";
-// import {TheAlert} from "@/components/TheAlert/index!"
+import { DialogDemo } from "@/components/TheDialog/index";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -9,16 +9,18 @@ function App() {
 	return (
 		<>
 			<div>
-				<div>count is {count}</div>
+				<span className="text-base">count is</span>
+				<span className="text-lg">{count}</span>
 			</div>
 			<div className="card">
-				<Button onClick={() => setCount((count) => count + 1)}>add 1</Button>
+				<Button onClick={() => setCount((count) => count + 1)}>add 1(normal style)</Button>
 				&nbsp;
-				<button onClick={() => setCount((count) => count + 2)}>add 2</button>
+				<button onClick={() => setCount((count) => count + 2)}>add 2(shadcn style)</button>
 			</div>
-			<p className="read-the-docs">
-        shadcn-app
-			</p>
+			<div>
+				<DialogDemo></DialogDemo>
+			</div>
+			<p className="read-the-docs">shadcn-app</p>
 		</>
 	);
 }
